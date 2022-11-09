3f683792374e03bc02024e918507b61cde2b14af
Paxian-Pi <paxian.pi@gmail.com>
2022-11-09 18:37:45 +0100

    updated

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = require('../../key_dev')
+    module.exports = require('../config/keys_prod')
 }
 else {
-    module.exports = require('../../key_dev')
+    module.exports = require('../config/keys_dev')
 }
