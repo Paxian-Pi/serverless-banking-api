abae7b1b86a3d30efe77c47a0b25491a0e1b9dbe
Paxian-Pi <paxian.pi@gmail.com>
2022-11-09 17:09:01 +0100

    updated

--- functions/server.js
+++ functions/server.js
@@ -66,7 +66,7 @@ app.use(passport.initialize())
 require('./config/passport')(passport)
 
 // Configure Db
-const db = require('./config/keys').mongoURI
+const db = require('./config/keys_dev').mongoURI
 
 // Connect to MongoDB
 mongoose.connect(db)
