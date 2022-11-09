a28236f4630ed832366c78c3ec7b50f860b9527b
Paxian-Pi <paxian.pi@gmail.com>
2022-11-09 17:03:10 +0100

    updated

---
+++ functions/config/key_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://BankinAPI:BankingAPI22@banking-app.qrxyt.mongodb.net/banking-app',
+    secretOrKey: 'secret'
+}
--- functions/config/keys_prod.js
+++ functions/config/keys_prod.js
@@ -1,2 +1,4 @@
-export const mongoURI = process.env.MONGO_URI;
-export const secretOrKey = process.env.SECRET_OR_KEY;
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
+}
